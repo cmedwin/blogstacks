55daf9eecb4357a6777421eab073c36000d7bbb4
Charlie Medwin <charlie@blogstacks.com>
2018-10-21 17:17:36 +0100

    New colour theme

--- blogstacks/scripts/main.js
+++ blogstacks/scripts/main.js
@@ -16,11 +16,3 @@ function myFunction() {
   }
 }
 
-function fix()
-{
-    var el = this;
-    var par = el.parentNode;
-    var next = el.nextSibling;
-    par.removeChild(el);
-    setTimeout(function() {par.insertBefore(el, next);}, 0)
-}
