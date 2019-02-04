b33beeb396d9337de159fe0ca06d11ace55254e2
cmedwin <c.medwin@live.co.uk>
2019-02-04 20:48:23 +0000

    Blogger search work in progress

--- blogstacks/scripts/main.js
+++ blogstacks/scripts/main.js
@@ -135,10 +135,55 @@ window.onload = function() {
         };
         // We can add as many individual inputs / tables as we want by just calling bindSearch with the right ids.
         function searchC() {
-        bindSearchC('#blog', '#myInput');
-        bindSearchC('#blogger', '#myInput');
+        bindSearchC('#blog', '#category');
+        bindSearchC('#blogger', '#category');
         }
         searchC();
+
+
+// 2b. Search - blogger - bind tables and input
+        var searchTableB = function searchTableB(table, input) {
+          alert("B");
+            // Since we bound the input, we can use input.value to get the current words typed into the input.
+            var filter = input.value.toUpperCase(),
+              // A table has both a thead and a tbody.
+              // By only selecting the tr nodes from the body, we can remove the entire 'check if this is a header tr logic of `tr.classList.contains('header')`
+              // Keep in mind that querySelector returns a nodeList, so if we want to use array methods, we need to covnert it into a real array.
+              // The original code uses getElementsByTagName, which return a LIVE nodeList, watch out for this difference.
+              rows = Array.prototype.slice.call(table.querySelectorAll('tbody tr.rowShow'));
+            rows.forEach(function(row) {
+              // Since we don't care in which cell the fitler is contained, we can just check the innerHTML of the entire row.
+              // This will only fail if the filter typed into the inputs is either 'tr' or 'td'
+              var hide = (row.innerHTML.toUpperCase().indexOf(filter) === -1);
+              // The alternative is actually checking each cell, but this makes the script take longer:
+              // var hide = !Array.prototype.slice.call( row.querySelectorAll('td') ).some(function( cell ) {
+              //     return (cell.innerHTML.indexOf( filter ) !== -1);
+              // });
+              if (hide) {
+                row.classList.add('fade'); 
+                //row.classList.remove('rowShow');
+              }
+              //else if (row.classList.contains('gone')) {
+                //row.classList.remove('gone');
+                //row.classList.add('rowShow');
+              //}
+            });
+          },
+          // helper function that we can use to bind the searchTable function to any table and input we want
+          // We add an onchange event listener, passing it a bound version of searchTable.
+          bindSearchB = function bindSearchB(tableID, inputID) {
+            var input = document.querySelector(inputID),
+              table = document.querySelector(tableID);
+            if (table && input) input.addEventListener('change', searchTableB.bind(null, table, input));
+            else alert('Table or input does not exist.');
+          };
+        // We can add as many individual inputs / tables as we want by just calling bindSearch with the right ids.
+        function searchB() {
+        bindSearchB('#blog', '#bloggerFilter');
+        bindSearchB('#blogger', '#bloggerFilter');
+        }
+        searchB();
+
 
 // 2c. Search - search box input - bind tables and input
       var searchTableI = function searchTableI(table, input) {
@@ -397,17 +442,22 @@ $("#shuffleMob").click(function(){
 
 function bloggerCatFilter(innerHTML) {
   var cat = innerHTML;
-  var headings = document.evaluate("//button[contains(., '" + cat + "')]", document, null, XPathResult.ANY_TYPE, null );
+  var headings = document.evaluate("//button[@class='btn' and contains(., '" + cat + "')]", document, null, XPathResult.ANY_TYPE, null );
   var thisHeading = headings.iterateNext();
   thisHeading.click();
 }
 
+
+// 11. Filter on blogger in blogger stack tiles & blog author tiles
+
 function bloggerFilter(innerHTML) {
+  document.getElementById("bloggerFilter").value = "";
+  searchB();
   alert(innerHTML);
 }
 
 
-// 11. Share button - currently displays variables for search
+// XX. Share button - currently displays variables for search
 
   function shareBut() {
     var category = document.getElementById("category").value;
